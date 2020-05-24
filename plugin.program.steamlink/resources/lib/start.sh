34bbecc26dca4d0ed3a53137a4d994455544fdbf
Pinembour <pinembour@teppelin.fr>
2020-05-24 12:56:29 +0000

    Switch from openvt to minimal X11 server for buster and Rpi4 compatibility

--- plugin.program.steamlink/resources/lib/start.sh
+++ plugin.program.steamlink/resources/lib/start.sh
@@ -2,7 +2,7 @@
 
 DIRNAME=$( dirname "${BASH_SOURCE[0]}" )
 
-sudo openvt -c 7 -s -f -- su kodi -c "bash ${DIRNAME}/heartbeat.sh & /usr/bin/steamlink &"
+bash ${DIRNAME}/heartbeat.sh & startx /usr/bin/steamlink &
 sudo systemctl stop kodi
 
 exit
